a93cd9350ef92af266824e490f01916076c0bcaf
nakashimaiori <ioringo072869@gmail.com>
2020-12-15 11:07:58 +0900

    before hd

--- welcome.php
+++ welcome.php
@@ -1,3 +1,10 @@
 <?php
-echo 'Welcome to PHP';
+
+$purpose = "Webプログラミング";
+
+echo "PHPとは、HTMLへの埋め込み型プログラミング言語です。<br />\n";
+echo "正式名称は「PHP: Hypertext Preprocessor」です。<br />\n";
+echo "PHPは{$purpose}に利用されます。<br />\n";
+echo "<a href=\"\">詳細を読む<a>\n";
+
 ?>
