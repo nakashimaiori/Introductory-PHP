69700904fe032fe877a61d664440f4a90a4da097
nakashimaiori <ioringo072869@gmail.com>
2020-12-15 11:09:33 +0900

    after heredoc

--- welcome.php
+++ welcome.php
@@ -2,9 +2,11 @@
 
 $purpose = "Webプログラミング";
 
-echo "PHPとは、HTMLへの埋め込み型プログラミング言語です。<br />\n";
-echo "正式名称は「PHP: Hypertext Preprocessor」です。<br />\n";
-echo "PHPは{$purpose}に利用されます。<br />\n";
-echo "<a href=\"\">詳細を読む<a>\n";
+echo <<< EOM
+PHPとは、HTMLへの埋め込み型プログラミング言語です。<br />
+正式名称は「PHP: Hypertext Preprocessor」です。<br />
+PHPは{$purpose}に利用されます!<br />
+<a href="">詳細を読む<a>
+EOM;
 
 ?>
