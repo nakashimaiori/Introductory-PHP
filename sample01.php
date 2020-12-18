f53d63eb3318166e3a332891a1155783f001627a
nakashimaiori <ioringo072869@gmail.com>
2020-12-18 12:15:03 +0900

    add_sample01.php

---
+++ sample01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>udemy</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <?php
+   print ('PHP勉強中');
+  ?>
+</body>
+</html>
