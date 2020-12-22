6ca8539dcaa47d57d5a267c88929aa62bd776ba2
nakashimaiori <ioringo072869@gmail.com>
2020-12-22 17:02:00 +0900

    add sample02

---
+++ sample02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>udemy</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <?php
+   $test_1 = 123;
+   $test_2 = 456;
+
+   $test_3 = $test_1 . $test_2;
+
+   var_dump($test_3);
+  ?>
+
+
+</body>
+</html>
